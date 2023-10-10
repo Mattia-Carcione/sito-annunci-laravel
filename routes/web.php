9e5a1c170037a3171e7a203211fde80827759251
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-10-10 19:10:22 +0200

    add migration database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('announcements', AnnouncementController::class);
+Route::resource('categories', CategoryController::class);
