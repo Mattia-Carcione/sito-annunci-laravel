640f849ca20ebd330d843c2606a0dedafb579135
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-10-11 18:31:20 +0200

    messo il componente nav nel template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,6 @@
 <x-layout>
 
     <main id="page-top">
-        <!-- Navigation-->
-        <x-nav/>
         <!-- Masthead-->
         <header class="masthead header-color-personal text-white text-center">
             <div class="container d-flex align-items-center flex-column">
