4d850e4fb78b6fe15dae8df50b94dedfcdf65d2a
Mattia-Carcione <mattia.carcione01@gmail.com>
2023-10-12 21:08:05 +0200

    implementato scale transition card

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,38 +1,38 @@
 <x-layout>
     <div class="container mt-5 pt-5">
         <h1 class="pt-5 text-center">Annunci</h1>
-        <div class="row">
+        <div class="row justify-content-start">
             @forelse ($announcements as $announcement)
-            <div class="product-card col-12 col-md-4 m-2">
-                @if ($announcement->created_at->diffInHours(now()) <= 24)
-                    <div class="badge rounded-pill mb-2" style="background-color: #5478F0;">New</div>
-                @endif
+                <div class="product-card col-12 col-md-3 m-2">
+                    @if ($announcement->created_at->diffInHours(now()) <= 24)
+                        <div class="badge rounded-pill mb-2" style="background-color: #5478F0;">New</div>
+                    @endif
 
-                <div class="product-tumb">
-                    <img src="https://www.venetoformazione.it/wp-content/uploads/2022/02/ottimizzare-immagini-display-retina.jpg"
-                        alt="">
-                </div>
-                <div class="product-details">
-                    <span class="product-catagory">{{ $announcement->category->name }}</span>
-                    <h4><a style="color: #5478F0"
-                            href="{{ route('announcements.show', $announcement) }}">{{ $announcement->title }}</a>
-                    </h4>
-                    <p>
-                        {{ $announcement->description }}
-                    </p>
-                    <div class="product-bottom-details">
-                        <div class="product-price">€{{ $announcement->price }}</div>
-                        <div class="product-links">
-                            <a href=""><i class="fa fa-heart"></i></a>
-                            <a href=""><i class="fa fa-shopping-cart"></i></a>
+                    <div class="product-tumb">
+                        <img src="https://www.venetoformazione.it/wp-content/uploads/2022/02/ottimizzare-immagini-display-retina.jpg"
+                            alt="">
+                    </div>
+                    <div class="product-details">
+                        <span class="product-catagory">{{ $announcement->category->name }}</span>
+                        <h4><a style="color: #5478F0"
+                                href="{{ route('announcements.show', $announcement) }}">{{ $announcement->title }}</a>
+                        </h4>
+                        <p>
+                            {{ $announcement->description }}
+                        </p>
+                        <div class="product-bottom-details">
+                            <div class="product-price">€{{ $announcement->price }}</div>
+                            <div class="product-links">
+                                <a href=""><i class="fa fa-heart"></i></a>
+                                <a href=""><i class="fa fa-shopping-cart"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             @empty
                 <p>Non ci sono annunci</p>
             @endforelse
-            {{$announcements->links()}}
+            {{ $announcements->links() }}
         </div>
 
     </div>
