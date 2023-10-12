e0d6f3aea2ba0c590da48abade8f941768f549ee
Andrea Criscione <andreacriscione96@hotmail.com>
2023-10-12 17:05:02 +0200

    creazione card in categories.show

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,35 +1,56 @@
 <x-layout>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <section class="py-5">
+        <div class="container px-5 my-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="text-center">
+                        <h2 class="fw-bolder">Esplora la Categoria: {{ $category->name }}</h2>
 
-    <a id="view-code" href="https://codepen.io/virgilpana/pen/RNYQwB" target="_blank">VIEW CODE</a>
+                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur
+                            adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
 
-    <div id="make-3D-space">
-        <div id="product-card">
-            @forelse ($category->announcements as $announcement)
-                <div id="product-front">
-                    <div class="shadow"></div>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png" alt="" />
-                    <div class="image_overlay"></div>
-                    <div id="view_details">View details</div>
-                    <div class="stats">
-                        <div class="stats-container">
-                            <span class="product_price">{{$announcement->price}}</span>
-                            <span class="product_name">{{$announcement->title}}</span>
-                            <p>Men's running shirt</p>
-
-
-                        </div>
                     </div>
                 </div>
+                <div class="d-flex justify-content-center align-items-center min-vh-100">
+                    <h1></h1>
+                </div>
 
+
+                <a id="view-code" href="https://codepen.io/virgilpana/pen/RNYQwB" target="_blank">VIEW CODE</a>
+                <div id="make-3D-space" class="d-flex justify-content-around">
+                    <div id="product-card" class="row gx-5">
+                        @forelse ($category->announcements as $announcement)
+                            <div id="product-front" class="col-lg-4 mb-5">
+
+                                <div class="shadow"></div>
+                                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png"
+                                    alt="" />
+                                <div class="image_overlay"></div>
+                                <div id="view_details">View details</div>
+                                <div class="stats">
+                                    <div class="stats-container">
+                                        <span class="product_price">{{ $announcement->price }}€</span>
+                                        <span class="product_name">{{ $announcement->title }}</span>
+                                        <span class="product_name">Descrizione: {{ $announcement->description }}</span>
+                                        <p class="card-footer my-5">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}} - Autore: {{$announcement->user->name ?? ''}}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+            @empty
+                <div>
+                    <h2>Non sono presenti Annunci per questa categoria</h2>
+                    <h3>Pubblicane uno: <a href="{{ route('announcements.create') }}" class="btn btn-success">Nuovo
+                            Annuncio</a>
+                    </h3>
+                </div>
+                @endforelse
+
+
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            </div>
         </div>
-    </div>
-@empty
-<div class="col-12">
-    <p></p>
-</div>
-    @endforelse
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    </section>
 </x-layout>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-color-personal text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="#page-top">FindEasy</a>
+        <a class="navbar-brand" href="{{route('homepage')}}">FindEasy</a>
         <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
             data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
             aria-expanded="false" aria-label="Toggle navigation">
@@ -10,7 +10,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Home</a>
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{route('homepage')}}">Home</a>
                 </li>
                 <li class="nav-item mx-0 mx-lg-1">
                     <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{route('announcements.index')}}">Annunci</a>
