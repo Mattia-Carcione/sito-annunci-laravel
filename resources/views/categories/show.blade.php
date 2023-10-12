3ae21d6d0f790a6646c36a739cedead05c8aa8cc
Andrea Criscione <andreacriscione96@hotmail.com>
2023-10-12 19:15:15 +0200

    Modifica card in categories.show

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -2,25 +2,12 @@
     <section class="py-5">
         <div class="container px-5 my-5">
             <div class="row gx-5 justify-content-center">
-                <div class="col-lg-8 col-xl-6">
-                    <div class="text-center">
-                        <h2 class="fw-bolder">Esplora la Categoria: {{ $category->name }}</h2>
-
-                        <p class="lead fw-normal text-muted mb-5">Lorem ipsum, dolor sit amet consectetur
-                            adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
-
-                    </div>
-                </div>
-                <div class="d-flex justify-content-center align-items-center min-vh-100">
-                    <h1></h1>
-                </div>
-
-
+                <h2 class="fw-bolder text-center">Esplora la Categoria: {{ $category->name }}</h2>
                 <a id="view-code" href="https://codepen.io/virgilpana/pen/RNYQwB" target="_blank">VIEW CODE</a>
                 <div id="make-3D-space" class="d-flex justify-content-around">
                     <div id="product-card" class="row gx-5">
                         @forelse ($category->announcements as $announcement)
-                            <div id="product-front" class="col-lg-4 mb-5">
+                            <div id="product-front" class="col-12 col-md-4 mb-5">
 
                                 <div class="shadow"></div>
                                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/245657/t-shirt.png"
