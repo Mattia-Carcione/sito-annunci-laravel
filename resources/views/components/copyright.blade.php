41f688f30731ac288dfc1d18b79e9d47a1616d9d
Andrea Criscione <andreacriscione96@hotmail.com>
2023-10-12 21:46:15 +0200

    Modifica card immagine

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -1,3 +1,3 @@
 <div class="copyright py-4 text-center text-white">
-    <div class="container"><small>Copyright &copy; Your Website 2023</small></div>
+    <div class="container"><small>Copyright &copy; Gli Stagisti-2023</small></div>
 </div>
