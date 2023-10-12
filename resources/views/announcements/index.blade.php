f3083b607d4a8ed881bc89e4c23952e388fef184
Marco Cima <marcoskrome3@gmail.com>
2023-10-12 18:54:34 +0200

    card di prova inserita

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -1,19 +1,22 @@
 <x-layout>
-    @forelse ($announcements as $announcement )
-        <div class="col-12 col-md-4 my-4 m-5">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">{{$announcement->title}}</h5>
-                  <p class="card-text">{{$announcement->description}}</p>
-                  <p class="card-text">{{$announcement->price}}</p>
-                  <a href="{{route('announcements.show', compact('announcement'))}}" class="btn btn-primary">Dettagli</a>
-                </div>
-              </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-md-4 my-4 p-5">
+                @forelse ($announcements as $announcement)
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://fastly.picsum.photos/id/1037/200/200.jpg?hmac=MMp-F1917NlyyHn_Bd0ZS6iR3v1-FLpGFkxYeQguinM" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $announcement->title }}</h5>
+                            <p class="card-text">{{ $announcement->description }}</p>
+                            <p class="card-text">{{ $announcement->price }}</p>
+                            <a href="{{ route('announcements.show', compact('announcement')) }}"
+                                class="btn btn-primary">Dettagli</a>
+                        </div>
+                    </div>
+                @empty
+                    <p>Non ci sono annunci</p>
+            </div>
         </div>
-    @empty
-    <div class="col-12 col-md-4 my-4">
-        <p>Non ci sono annunci</p>
+        @endforelse
     </div>
-    @endforelse
 </x-layout>
