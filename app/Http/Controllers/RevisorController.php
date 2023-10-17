d05077048d45b7cbb516fb138c506f6f4b8bb001
Mattia <mattia@MacBook-Pro-di-Mattia.local>
2023-10-17 17:14:59 +0200

    add mail becomerevisor

--- app/Http/Controllers/RevisorController.php
+++ app/Http/Controllers/RevisorController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\BecomeRevisor;
 use App\Models\Announcement;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class RevisorController extends Controller
 {
@@ -29,5 +34,12 @@ class RevisorController extends Controller
 
     public function becomeRevisor()
     {
+     Mail::to('admin@findeasy.it')->send(new BecomeRevisor(Auth::user())); 
+     return redirect()->back()->with('message', 'Complimenti, hai richiesto di diventare revisore correttamente'); 
+    }
+
+    public function makeRevisor(User $user){
+        Artisan::call('FindEasy:makeUserRevisor', ["email" => $user->email]);
+        return redirect('/')->with('message', 'Complimenti! l\'utente è diventato revisore');
     }
 }
