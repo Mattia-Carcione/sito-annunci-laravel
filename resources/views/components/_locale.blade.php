7eb1f73bec17a1a036ef78f48da7f88630e4cf0c
Marco Cima <marcoskrome3@gmail.com>
2023-10-30 17:32:59 +0100

    Modifiche navbar lingue e login e registrati

--- resources/views/components/_locale.blade.php
+++ resources/views/components/_locale.blade.php
@@ -3,7 +3,10 @@
     @csrf
 
     <button type="submit" class="nav-link" style="background-color:transparent; border:none;">
+        <div class="container d-flex">
         <span class="flag-icon flag-icon-{{$nation}}"></span>
+        <span class="mx-3 text-dark">{{$text}}</span>
+    </div>
     </button>
 
 
--- resources/views/components/nav2.blade.php
+++ resources/views/components/nav2.blade.php
@@ -30,14 +30,6 @@
                         </ul>
                     </div>
                 </li>
-                @guest
-                    <li class="nav-item px-1">
-                        <a class="nav-link nav-a" href="{{ route('register') }}">{{ __('ui.register') }}</a>
-                    </li>
-                    <li class="nav-item px-1">
-                        <a class="nav-link nav-a" href="{{ route('login') }}">Login</a>
-                    </li>
-                @endguest
 
                 @auth
 
@@ -59,51 +51,64 @@
                             </a>
                         </li>
                     @endif
-                <div class="d-flex align-items-center justify-content-end" style="flex: 1; padding-right: 1rem ">
-                    <li class="nav-item d-flex align-items-center dropdown">
-                        <a class="nav-link dropdown-toggle nav-a text-light" href="#" id="#" role="button"
-                            data-bs-toggle="dropdown">{{ Auth::user()->name }}</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item nav-a" href="#">{{ __('ui.profile') }}</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item nav-a" href="#"
-                                    onclick="event.preventDefault(); getElementById('form').submit();">
-                                    Logout</a></li>
-                            <form id="form" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                            </form>
-                        </ul>
-                    </li>
+                    <div class="d-flex align-items-center justify-content-end" style="flex: 1; padding-right: 1rem ">
+                        <li class="nav-item d-flex align-items-center dropdown">
+                            <a class="nav-link dropdown-toggle nav-a text-light" href="#" id="#"
+                                role="button" data-bs-toggle="dropdown">{{ Auth::user()->name }}</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item nav-a" href="#">{{ __('ui.profile') }}</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item nav-a" href="#"
+                                        onclick="event.preventDefault(); getElementById('form').submit();">
+                                        Logout</a></li>
+                                <form id="form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                </form>
+                            </ul>
+                        </li>
                     @endauth
 
                     <li class="nav-item">
 
                         <div class="dropdown">
-                            <a class=" dropdown-toggle nav-link nav-a text-light" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a class=" dropdown-toggle nav-link nav-a text-light" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
                                 {{ __('ui.language') }}
                             </a>
-    
+
                             <ul class="dropdown-menu">
-    
-                                <li class=" nav-a">
-                                    <x-_locale lang='it' nation='it' />
+
+                                <li class=" nav-a d-flex align-items-center">
+                                    <x-_locale lang='it' nation='it' text='Italiano'/>
                                 </li>
-    
-                                <li class=" nav-a">
-                                    <x-_locale lang='en' nation='gb' />
+
+                                <li class=" nav-a d-flex align-items-center">
+                                    <x-_locale lang='en' nation='gb' text='English' />
                                 </li>
-    
-                                <li class=" nav-a">
-                                    <x-_locale lang='es' nation='es' />
+
+                                <li class=" nav-a d-flex align-items-center">
+                                    <x-_locale lang='es' nation='es' text='Español' />
                                 </li>
-    
+
                             </ul>
                         </div>
                     </li>
                 </div>
+                @guest
+                    <div class="align-items-center justify-content-end" style="flex: 1; padding-right: 1rem ">
+                        <ul class="navbar-nav me-auto justify-content-end d-flex">
+                            <li class="nav-item  d-flex px-1">
+                                <a class="nav-link nav-a text-light"
+                                    href="{{ route('register') }}">{{ __('ui.register') }}</a>
+                            </li>
+                            <li class="nav-item  d-flex px-1">
+                                <a class="nav-link nav-a text-light" href="{{ route('login') }}">Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                @endguest
             </ul>
         </div>
     </div>
