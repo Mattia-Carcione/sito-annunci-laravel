b00a1b60ce075ee6662749af8a183e3023b7465d
Antonello Rinaldelli <michelino.am@gmail.com>
2023-11-02 20:51:59 +0100

    modifiche

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/public/assets/img/findeasy-high-resolution-logo-transparent.png" type="image/x-icon">
     @livewireStyles
     <title>FindEasy.com</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
